4273198265111909325e41a30f9068aafe18c754
ousmo1997 <koumareousmane97@gmail.com>
2022-10-12 09:09:04 +0000

    Exo1

---
+++ Exo4.py
@@ -0,0 +1,11 @@
+n1=int(input("Saisir n1: "))
+n2=int(input("Saisir n2: "))
+oper=input("Saisir l'opérateur: ")
+if oper=='+':
+    print(n1+n2)
+if oper=='-':
+    print(n1-n2)
+if oper=='*':
+    print(n1*n2)
+if oper=='/':
+    print(n1/n2)
